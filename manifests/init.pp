cd68e3c0adc776e6af40b13ae9967ef1b742ad8a
Frederic DROUET <fdrouet@exoplatform.com>
2011-12-07 18:13:44 +0100

    SWF-1662 + SWF-1628 : puppet module use the repo puppet module + clean doc

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,48 +1,124 @@
-# Class: puppet
+################################################################################
 #
-#   This module manages the puppet agent service.
+#   This module manages the puppet agent services (agent, master, dashboard).
 #
 #   Tested platforms:
+#    - Ubuntu 11.10 Oneiric
 #    - Ubuntu 11.04 Natty
 #    - Ubuntu 10.04 Lucid
 #
-# Parameters:
-#	$agent_auto_start:
-#		allow puppet agent service to start at boot time or not (true / false) (default: true)
-#	$lastversion:
-#		this variable allow to chose if the package should always be updated to the last available version (true) or not (false) (default: false)
+# == Parameters
 #
-# Actions:
-#  Installs, configures, and manages the puppet service.
+# [+lastversion+]
+#   (OPTIONAL) (default: false) 
+#   
+#   this variable allow to chose if the package should always be updated to the last available version (true) or not (false) (default: false)
 #
-# Requires:
+# [+agent_auto_start+]
+#   (OPTIONAL) (default: true)
+#   
+#   allow puppet agent service to start at boot time (true) or not (false)
 #
-# **Simple Agent Usage:**
+# [+agent_pp_dir+]
+#   (OPTIONAL) (default: /etc/puppet)
+#   
+#   The agent puppet parent directory specify where is stored the puppet configuration for agent
+#
+# [+agent_runinterval+] 
+#   (OPTIONAL) (default: 1800 => every 30 minutes)
+#   
+#   The frequency for the puppet agent to execute in seconds
+#
+# [+master+] 
+#   (OPTIONAL) (default: false)
+#   
+#   allow puppet master on this node (true) or not (false)
+#
+# [+master_auto_start+]
+#   (OPTIONAL) (default: true)
+#   
+#   allow puppet master service to start at boot time (true) or not (false)
+#
+# [+master_fqdn+]
+#   (OPTIONAL) (default: puppet.${::domain})
+#   
+#   the fully qualified domain name of the puppet master
+#
+# [+master_port+]
+#   (OPTIONAL) (default: 8140)
+#   
+#   the port used by puppet master to listen for agent connexions
+#
+# [+master_pp_dir+]
+#   (OPTIONAL) (default: /etc/puppet)
+#   
+#   The master puppet parent directory specify where is stored the puppet configuration for master
+#
+# [+master_dns_alt_name+]
+#   (OPTIONAL) (default: )
+#   
+#   a comma separated list of fully qualified name DNS aliases for the master nodes (it only make sens on the node with puppet master)
+#
+# [+dashboard+]
+#   (OPTIONAL) (default: false)
+#   
+#   allow puppet dashboard on this node (true) or not (false)
+#
+# [+dashboard_auto_start+]
+#   (OPTIONAL) (default: true)
+#   
+#   allow puppet dashboard service to start at boot time (true) or not (false)
+#
+# [+dashboard_fqdn+]
+#   (OPTIONAL) (default: dashboard.${::domain})
+#   
+#   the fully qualified domain name of the puppet dashboard
+#
+# [+dashboard_port+]
+#   (OPTIONAL) (default: 3000)
+#   
+#   the port used by puppet dashboard to listen for puppet master connexions
+#
+# == Modules Dependencies
+#
+# [+repo+]
+#   the +repo+ puppet module is needed to :
+#   
+#   - install the puppetlab package repository and pupetlab repo GPG key (in puppet::install)
+#
+#   - refresh the repository before installing package (in puppet::install)
+#
+# == Examples
+#
+# === Simple Agent Usage
 #
 #   class { "puppet":
-#		agent_auto_start	=> true,
-#		lastversion			=> false,
-#		master				=> false,
-#		master_fqdn			=> puppet.$::domain,
+#       agent_auto_start    => true,
+#       lastversion         => false,
+#       master              => false,
+#       master_fqdn         => puppet.${::domain},
 #   }
 #
-# **Simple Agent and Master Usage:**
+# === Simple Agent and Master Usage
 #
 #   class { "puppet":
-#		agent_auto_start	=> true,
-#		lastversion			=> false,
-#		master				=> true,
-#		master_fqdn			=> $::fqdn,
+#       agent_auto_start    => true,
+#       lastversion         => false,
+#       master              => true,
+#       master_auto_start   => true,
+#       master_fqdn         => puppet.${::domain},
 #   }
 #
-# [Remember: No empty lines between comments and class definition]
+################################################################################
 class puppet (	$lastversion = false, 
 										$agent_auto_start 		= true, 																			$agent_pp_dir 	= "/etc/puppet",	$agent_runinterval = "1800",
-				$master 	= false, 	$master_auto_start 		= true, 	$master_fqdn 	= "puppet.$::domain", 		$master_port 	= "8140", 	$master_pp_dir	= "/etc/puppet",	$master_dns_alt_name = "",
-				$dashboard 	= false, 	$dashboard_auto_start 	= true, 	$dashboard_fqdn = "dashboard.$::domain", 	$dashboard_port	= "3000" 
+				$master 	= false, 	$master_auto_start 		= true, 	$master_fqdn 	= "puppet.${::domain}", 	$master_port 	= "8140", 	$master_pp_dir	= "/etc/puppet",	$master_dns_alt_name = "",
+				$dashboard 	= false, 	$dashboard_auto_start 	= true, 	$dashboard_fqdn = "dashboard.${::domain}", 	$dashboard_port	= "3000" 
 			 ) {
 	
 	# TODO Implement Apache Passenger support for Puppet Master (need vhost and conf.d feature of new apache2 module - SWF-1621)  
+	
+	include repo
 	
 	# parameters validation
 	if ($lastversion != true) and ($lastversion != false) {
--- manifests/install.pp
+++ manifests/install.pp
@@ -5,28 +5,47 @@ class puppet::install {
 	
 	case $::operatingsystem {
 		/(Ubuntu|Debian)/: {
-			file { "/etc/apt/sources.list.d/puppet.list":
-		    	content => template("puppet/sources.list.puppet.erb"),
-	    	}
+            repo::define { "puppetlab-repo":
+                file_name   => "puppetlab",
+                url         => "http://apt.puppetlabs.com/ubuntu",
+                sections    => ["main"],
+                source      => true,
+                key         => "4BD6EC30",
+                key_server  => "keyserver.ubuntu.com",
+                notify      => Exec["repo-update"],
+            }
+            
+            # this fil eremover is here to clean up puppet.list manually added during system installation
+            repo::define { "puppetlab-oldfile":
+                file_name   => "puppet",
+                url         => "http://apt.puppetlabs.com/ubuntu",
+                sections    => ["main"],
+                source      => true,
+                installed   => false,
+                notify  => Exec["repo-update"],
+            }
 	    }
     }
     
     # Agent packages
 	package { ["puppet", "facter"]: 
-		ensure => $puppet::params::ensure_mode, 
+        ensure    => $puppet::params::ensure_mode, 
+        require   => [ Exec ["repo-update"], Repo::Define [ "puppetlab-repo" ] ],
 	}
 
     # Master packages
 	if ( $puppet::master == true ) {
 		package { "puppetmaster": 
-			ensure => $puppet::params::ensure_mode, 
+            ensure      => $puppet::params::ensure_mode, 
+            require   => [ Exec ["repo-update"], Repo::Define [ "puppetlab-repo" ] ],
 		}
 	}
 
     # Dashboard packages
 	if ( $puppet::dashboard == true ) {
 		package { "puppet-dashboard": 
-			ensure => $puppet::params::ensure_mode, 
+            ensure  => $puppet::params::ensure_mode, 
+            require   => [ Exec ["repo-update"], Repo::Define [ "puppetlab-repo" ] ],
 		}
 	}
 }
