036d60746c7484d24d968cf7a9f4c154ede0c410
bourasom <obouras@exoplatform.com>
2019-07-16 18:11:12 +0200

    ITOP_4199: move libaugeas-ruby to ruby-augeas

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,7 +5,7 @@ class puppet::install {
   case $::operatingsystem {
     /(Ubuntu|Debian)/ : {
       case $::lsbdistrelease {
-        /(12.04|14.04|16.04)/ : {
+        /(12.04|14.04)/ : {
 
           apt::source { 'puppetlabs':
             location => 'http://apt.puppetlabs.com',
@@ -26,6 +26,33 @@ class puppet::install {
           ensure_packages ([
             'libaugeas-ruby1.8',
             'libaugeas-ruby1.9.1',
+            'augeas-tools',
+            'augeas-lenses',
+            'puppet',
+            'facter'
+          ], {
+            'ensure'  => $puppet::params::ensure_mode,
+            'require' => [Apt::Source['puppetlabs'],Class['apt::update']]
+          } )
+        }
+        /(16.04)/ : {
+
+          apt::source { 'puppetlabs':
+            location => 'http://apt.puppetlabs.com',
+            repos    => 'main dependencies',
+            key      => {
+              'id'     => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
+              'server' => 'pgp.mit.edu',
+            },
+          }
+          # Agent packages
+          # ruby-augeas installed instead of libaugeas-ruby
+          # ensure_packages ( 'augeas-tools',         { 'ensure' => $puppet::params::ensure_mode, 'require' => [Apt::Source['puppetlabs'],Class['apt::update'] } )
+          # ensure_packages ( 'augeas-lenses',        { 'ensure' => $puppet::params::ensure_mode, 'require' => [Apt::Source['puppetlabs'],Class['apt::update'] } )
+          # ensure_packages ( 'puppet', { 'ensure' => $puppet::params::ensure_mode, 'require' => Apt::Source['puppetlabs'] } )
+          # ensure_packages ( 'facter', { 'ensure' => $puppet::params::ensure_mode, 'require' => Apt::Source['puppetlabs'] } )
+
+          ensure_packages ([
             'augeas-tools',
             'augeas-lenses',
             'puppet',
